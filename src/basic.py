1cce044be3bc5ab8594566592bb71ea90e143a7a
Chad Upjohn <upjohnc@gmail.com>
2022-06-11 13:27:16 -0500

    removing unneeded packages

--- src/basic.py
+++ src/basic.py
@@ -3,12 +3,19 @@ import os
 import boto3
 from loguru import logger
 
+host_name = os.environ["LOCALSTACK_HOSTNAME"]
+# forcing localstack to show how to use with tests
+localstack = True
+endpoint_url = dict()
+if localstack:
+    endpoint_url = dict(endpoint_url=f"http://{host_name}:4566")
+
 
 def run(event, context):
     logger.info(f"Event info: {event}")
     logger.info(os.environ.get("LOG_THRESHOLD"))
     logger.info("Useful")
-    client = boto3.client("dynamodb")
+    client = boto3.client("dynamodb", **endpoint_url)
     response = client.scan(TableName="users_table")
     logger.info(response)
     return response
