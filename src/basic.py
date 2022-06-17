e2ef6fb10246395484597b059bd91edbeecba8f2
Chad Upjohn <upjohnc@gmail.com>
2022-06-17 18:10:55 -0500

    correcting

--- src/basic.py
+++ src/basic.py
@@ -1,10 +1,10 @@
-import os
+# import os
 
 from loguru import logger
 
 
 def run(event, context):
-    logger.info(f"Event info: {event}")
-    logger.info(os.environ.get("LOG_THRESHOLD"))
+    # logger.info(f"Event info: {event}")
+    # logger.info(os.environ.get("LOG_THRESHOLD"))
     logger.info("Useful")
     return {"what": "big deal"}
